724d9681e4634691a83721489a7a409f605ca9a9
Joel David C <jdcruzzamora@gmail.com>
2021-03-18 22:32:00 -0600

    Status bar corregida

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        brightness: Brightness.dark,
         centerTitle: true,
         title: Text('Historial'),
         elevation: 0,
